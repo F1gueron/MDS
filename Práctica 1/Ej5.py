73bd13b9aa20a996a46af0e603adf8ba41aa05af
F1gueron <jfiguerasmarquez@gmail.com>
2025-02-27 18:30:36 +0100

    - Soy muy malo, ya funciona

--- Práctica 1/Ej5.py
+++ Práctica 1/Ej5.py
@@ -1,6 +1,6 @@
 import re
 
-pattern = r"\b(?:C\/|Calle)\s([A-ZÑÁÉÍÓÚÜ][a-záéíóúñü]{0,}),?\s*(?:(?:N|n)(?:º)|(?:N|n)\s?(?=\d))?\s?(\d+)\s*,\s*(\d{5})\b"
+pattern = r"\b(?:C\/|Calle)\s([A-ZÑÁÉÍÓÚÜ][a-záéíóúñü]{0,}),?\s{1,}(?:(?:N|n)(?:º)|(?:N|n)\s?(?=\d))?\s{0,}(\d+)\s{0,},\s{0,}(\d{5})\b"
 text = input()
 
 matches = re.findall(pattern, text)
--- Práctica 1/Ej6.py
+++ Práctica 1/Ej6.py
@@ -1,11 +1,10 @@
 import re
 
-text = input().strip()
-regex = r"^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{3}\s+(\w+)\s+(?:\d+)\s+---\s+\[(\w+)\]\s+(?:[\w]\.)*([\w]+)\s+:\s+(.+)$"
-match = re.fullmatch(regex, text)
+text = input()
+#print(text)
+#text = "2022-02-07 01:14:28.313  INFO 1174086 --- [main] drfp.Main            : Starting Main v0.1-SNAPSHOT using Java 17.0.1 on raul2-ubuntu with PID 1174086 started by rmartin"
+regex = r"\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{3}\s+(\w+)\s+(?:\d+)\s+---\s+\[(\w+)\]\s+(?:\w*\.)*(\w+)\s+:\s+(.+)"
+match = re.findall(regex, text)
 if match:
-    level = match.group(1)
-    thread = match.group(2)
-    class_name = match.group(3)
-    message = match.group(4)
-    print(f'"{level}","{thread}","{class_name}","{message}"')
+    for m in match:
+        print(f'"{m[0]}","{m[1]}","{m[2]}","{m[3]}"')
