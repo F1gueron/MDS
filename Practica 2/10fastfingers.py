451b6f280a193fb4a687ff4f9b550cd1a17b022a
Jorge Moreno <85845820+Iokete@users.noreply.github.com>
2025-03-18 14:20:28 +0100

    Create 10fastfingers.py

---
+++ Practica 2/10fastfingers.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+
+driver.get("http://192.168.59.128/")
+
+text_input = driver.find_element(By.ID, 'textInput')
+
+for i in range(1, 21):
+    word = driver.find_element(By.ID, f"word_{i}").text
+    text_input.send_keys(word + " ")
+
+
+time.sleep(30) # Sleep para ver la flag
