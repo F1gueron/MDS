ec325f8eea9cdaef10f744e3b802839767b8afb3
Jorge Moreno <85845820+Iokete@users.noreply.github.com>
2025-03-18 23:23:10 +0100

    Update blog.py

--- Practica 2/blog.py
+++ Practica 2/blog.py
@@ -31,22 +31,11 @@ while hrefs:
                     and new_href not in visited):
                 hrefs.append(link.get_attribute("href"))
         try:
-            #page_text = driver.page_source # Descomentar esto para buscar en el código fuente de cada página
             page_text = driver.find_element(By.CLASS_NAME, "article-post").text # Para buscar únicamente en el cuerpo del post
-            #page_text += driver.find_element(By.CLASS_NAME, "posttitle").text # Para buscar en el cuerpo del post y en el título
             count += len(re.findall(r"\bURJC\b", page_text)) # Para buscar sólo lo que tenga URJC o URJC.
-            #count += len(re.findall(r"\w*URJC\w*[.,]*", page_text)) # Para buscar URJC y otras variaciones como asdasdURJC o URJCasdad
         except Exception as e:
             pass
 
 print(f"\nNúmero total de apariciones de \"URJC\": {count}")
 
 driver.quit()
-
-# Usando la regex más amplia y el page_source = 280 -> INCORRECTO en ctfd
-# Regex menos amplia y page_source = 273 -> INCORRECTO en ctfd
-# Regex menos amplia y solo cuerpo del post = 271 -> Este
-# Regex más amplia y solo cuerpo del post = 278 -> no lo sabemos aun
-# Regex más amplia y cuerpo del post + título = 278 -> no lo sabemos aun
-# Regex menos amplia y cuerpo del post + título = 271 -> no lo sabemos aun
-# En los títulos no hay URJC, por lo que no afecta a los resultados
